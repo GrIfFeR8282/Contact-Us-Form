2dbfb0f86c1bde0bf73433b5129b403154f8902f
GrIfFeR8282 <griffer8282@gmail.com>
2026-05-24 15:32:49 -0300

    Disabled strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.css'
-import App from './pages/index/index.jsx'
+import Index from './pages/index/index.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Index />
 )
